efa940a631a952a8f6533fa84f55ee69ffad4d25
JaNord <janordma@abo.fi>
2020-09-14 12:13:50 +0300

    Updates

---
+++ precache-manifest.566e4ccd063fdc4627ab260d85f80063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334726abf11c8d10c676d701e9e270c4",
+    "url": "/country-info/index.html"
+  },
+  {
+    "revision": "ff2601ab9f28889ae3dd",
+    "url": "/country-info/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75d9347790fbbb788b8c",
+    "url": "/country-info/static/js/2.73c694dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-info/static/js/2.73c694dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2601ab9f28889ae3dd",
+    "url": "/country-info/static/js/main.e83c1784.chunk.js"
+  },
+  {
+    "revision": "6570664c29cbf3c233fe",
+    "url": "/country-info/static/js/runtime-main.30025fcc.js"
+  }
+]);
